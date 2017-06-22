b812f5c652ef0b5e971b46d50e019f553c12ac99
Chris Palmer <cpalmer608@gmail.com>
2017-06-22 18:36:42 -0500

    posts scoped to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
 require 'random_data'
 
+#Create Users
+5.times do
+  User.create!(
+    name:     RandomData.random_name,
+    email:    RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 # Create Topics
 15.times do
   Topic.create!(
@@ -12,9 +22,10 @@ topics = Topic.all
 #Create Posts
 50.times do
   Post.create!(
+    user:   users.sample,
     topic:  topics.sample,
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
   )
 end
 posts = Post.all
@@ -27,7 +38,14 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'cpalmer608@gmail.ccom',
+  password: 'helloworld'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
